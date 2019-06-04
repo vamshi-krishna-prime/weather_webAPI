f135af7f8aa89104cb13cf07d8291a9ce2b28557
vamshi krishna prime <vamshi.krishna.prime@gmail.com>
2019-06-04 22:58:28 +0530

    Update weather_request.py

--- weather_request.py
+++ weather_request.py
@@ -1,9 +1,6 @@
 import requests
 if __name__ == "__main__":
     try:
-        # the url is entered wrong intentionally.
-        # correct url is:
-        # "https://www.metaweather.com/api/location/search/?query=london"
         r = requests.get("https://www.Wmetaweather.com/api/"
                          "location/search/?query=london")
         print(r.text)
