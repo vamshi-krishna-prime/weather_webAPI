7f78d30d4d24d19fdb2ce39fc403402f6a14053c
vamshi krishna prime <vamshi.krishna.prime@gmail.com>
2019-06-04 22:59:15 +0530

    Update and rename weather_request.py to weather_request_simple.py

--- weather_request.py
+++
@@ -1,8 +0,0 @@
-import requests
-if __name__ == "__main__":
-    try:
-        r = requests.get("https://www.Wmetaweather.com/api/"
-                         "location/search/?query=london")
-        print(r.text)
-    except requests.exceptions.ConnectionError:
-        print("The server is not responding....")
